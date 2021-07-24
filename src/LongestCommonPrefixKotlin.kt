8fcb41587c5f7309c88e887d3d27b05e3321d0ca
谭嘉俊 <1120571286@qq.com>
2021-07-24 23:56:35 +0800

    14. 最长公共前缀（Longest Common Prefix）。

---
+++ src/LongestCommonPrefixKotlin.kt
@@ -0,0 +1,19 @@
+/**
+ * Created by TanJiaJun on 2021/7/24.
+ * 14. 最长公共前缀（Longest Common Prefix）
+ * 难度：简单
+ *
+ * @see <a href="https://leetcode-cn.com/problems/longest-common-prefix/">Longest Common Prefix</a>
+ */
+object LongestCommonPrefixKotlin {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+
+    }
+
+    private fun longestCommonPrefix(strs: Array<String>): String {
+
+    }
+
+}
